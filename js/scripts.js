3b51d2a64f577fae9f892181e534e1519b60e227
Andrea Chiotti <andreamchiotti@gmail.com>
2020-05-04 17:03:17 -0700

    1st if statment that didn't work

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,19 @@
       event.preventDefault();
 
   var nameInput = $("input#userName").val();
+  var levelInput = $("#ideal").val();
+  var thinkingInput = $("#thinking").val();
+  var hpHouseInput = $("input:radio[name=hpHouse]:checked").val();
+  var moneyInput = $("#money").val();
+  var musicInput = $("input:radio[name=music]:checked").val();
 
+  if ((levelInput === "alot")  && (thinkingInput === "concrete") && (moneyInput === "trillions")) {
+    var result = "C";
+    $("#languageResult").text(result);
   
   
   $(".userName").text(nameInput);
-
-
-
+  
   $(".showResult").show();
   });
 });
